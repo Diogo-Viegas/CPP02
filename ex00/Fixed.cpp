002d737c8ad5aae27c19e466a343a296ebcf23ed
Diogo Viegas <diogo.dc.viegas@gmail.com>
2026-05-26 20:24:22 +0100

    ex00 done

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -5,10 +5,33 @@ Fixed::Fixed()
     _number = 0;
     std::cout << "Default constructor called" << std::endl;
 }
-Fixed &Fixed::operator=(const Fixed &obj)
+
+Fixed::Fixed(const Fixed &obj)
 {
-    if (this != &obj)
-        _number = obj._number;
+    std::cout << "Copy constructor called " << std::endl;
+    *this = obj;
+}
+Fixed& Fixed::operator=(const Fixed &obj)
+{
     std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &obj)
+        _number = obj.getRawBits();
     return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return _number;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    _number = raw;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
 }
